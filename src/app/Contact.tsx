51747cbec9cc9db42ed1f4983ef61dafdb764e7d
pixel-glyph <arjbattaglia@gmail.com>
2023-04-13 21:39:29 -0400

    Build and style Contact section

--- src/app/Contact.tsx
+++ src/app/Contact.tsx
@@ -1,16 +1,26 @@
+import Image from 'next/image';
+import styles from './Contact.module.scss';
+
+import envelopeIcon from '../../public/envelope.svg';
+
 export default function Contact() {
   return (
-    <section>
+    <section className={styles.section}>
       <h2>Get in Touch</h2>
-      <p>I am currently available for hire.</p>
-      <p>
+      <p className={styles.info}>I am currently available for hire.</p>
+      <p className={styles.info}>
         If you’re on the lookout for a driven, best practices-following,
         detail-oriented frontend developer, look no further!
       </p>
-      {/* button to open form modal */}
-      <button>
-        {/* letter SVG */}
-        <span>MESSAGE ME</span>
+      <button className={styles.button}>
+        <Image
+          src={envelopeIcon}
+          alt=""
+          width={33}
+          height={22}
+          className={styles.icon}
+        />
+        <span className={styles.buttonText}>MESSAGE ME</span>
       </button>
     </section>
   );
